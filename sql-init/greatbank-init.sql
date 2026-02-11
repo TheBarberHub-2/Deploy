29d7a1181b75762b1f43752e9339e03370ef9528
PBC5 <160127358+PBC5@users.noreply.github.com>
2026-02-11 12:58:43 +0100

    Revision de BD

--- sql-init/greatbank-init.sql
+++ sql-init/greatbank-init.sql
@@ -3,56 +3,56 @@ CREATE DATABASE IF NOT EXISTS banco_barber;
 USE banco_barber;
 
 CREATE TABLE clientes (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    login VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
-    apellido1 VARCHAR(50) NOT NULL,
-    apellido2 VARCHAR(50),
-    dni VARCHAR(9) NOT NULL UNIQUE,
-    api_token VARCHAR(255)
+    id 						BIGINT PRIMARY KEY AUTO_INCREMENT,
+    login 					VARCHAR(50) NOT NULL UNIQUE,
+    password 				VARCHAR(100) NOT NULL,
+    nombre 					VARCHAR(50) NOT NULL,
+    apellido1 				VARCHAR(50) NOT NULL,
+    apellido2 				VARCHAR(50),
+    dni 						VARCHAR(9) NOT NULL UNIQUE,
+    api_token 				VARCHAR(255)
 );
 
 CREATE TABLE cuentas_bancarias (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    cliente_id BIGINT NOT NULL,
-    iban VARCHAR(50) NOT NULL UNIQUE,
-    saldo DECIMAL(38,2) NOT NULL,
-FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
+    id 						BIGINT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id 			BIGINT NOT NULL,
+    iban 					VARCHAR(50) NOT NULL UNIQUE,
+    saldo 					DECIMAL(38,2) NOT NULL,
+	 FOREIGN KEY 			(cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
 );
 
 CREATE TABLE tarjetas_credito (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    cuenta_id BIGINT NOT NULL,
-    numero_tarjeta VARCHAR(30) NOT NULL UNIQUE,
-    fecha_caducidad VARCHAR(10) NOT NULL,
-    cvc VARCHAR(3) NOT NULL,
-    nombre_completo VARCHAR(100) NOT NULL,
-    FOREIGN KEY (cuenta_id) REFERENCES cuentas_bancarias(id) ON DELETE CASCADE
+    id 						BIGINT PRIMARY KEY AUTO_INCREMENT,
+    cuenta_id 				BIGINT NOT NULL,
+    numero_tarjeta 		VARCHAR(30) NOT NULL UNIQUE,
+    fecha_caducidad 		VARCHAR(10) NOT NULL,
+    cvc 						VARCHAR(3) NOT NULL,
+    nombre_completo 		VARCHAR(100) NOT NULL,
+    FOREIGN KEY 			(cuenta_id) REFERENCES cuentas_bancarias(id) ON DELETE CASCADE
 );
 
 CREATE TABLE movimientos_bancarios (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-cuenta_id BIGINT NOT NULL,
-    tipo_movimiento ENUM('DEBE', 'HABER') NOT NULL,
-    origen_movimiento ENUM('TRANSFERENCIA', 'DOMICILIACION', 'TARJETA_BANCARIA') NOT NULL,
-    tarjeta_credito_id BIGINT,
-    fecha DATETIME(6) NOT NULL,
-    importe DECIMAL(38,2) NOT NULL,
-    concepto VARCHAR(255) NOT NULL,
-    FOREIGN KEY (tarjeta_credito_id) REFERENCES tarjetas_credito(id),
-    FOREIGN KEY (cuenta_id) REFERENCES cuentas_bancarias(id) ON DELETE CASCADE
+    id 						BIGINT PRIMARY KEY AUTO_INCREMENT,
+	 cuenta_id				BIGINT NOT NULL,
+    tipo_movimiento 		ENUM('DEBE', 'HABER') NOT NULL,
+    origen_movimiento 	ENUM('TRANSFERENCIA', 'DOMICILIACION', 'TARJETA_BANCARIA') NOT NULL,
+    tarjeta_credito_id 	BIGINT,
+    fecha 					DATETIME(6) NOT NULL,
+    importe 				DECIMAL(38,2) NOT NULL,
+    concepto 				VARCHAR(255) NOT NULL,
+    FOREIGN KEY 			(tarjeta_credito_id) REFERENCES tarjetas_credito(id),
+    FOREIGN KEY			(cuenta_id) REFERENCES cuentas_bancarias(id) ON DELETE CASCADE
 );
 
 CREATE TABLE sesion (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-usuario_id BIGINT NOT NULL,
-token VARCHAR(100) UNIQUE NOT NULL,
-expired_date DATE,
-FOREIGN KEY (usuario_id) REFERENCES clientes(id) ON DELETE CASCADE
+	id					BIGINT PRIMARY KEY AUTO_INCREMENT,
+	usuario_id		BIGINT NOT NULL,
+	token				VARCHAR(100) UNIQUE NOT NULL,
+	expired_date	DATE,
+	FOREIGN KEY		(usuario_id) REFERENCES clientes(id) ON DELETE CASCADE
 );
 
-INSERT INTO clientes (login, password, nombre, apellido1, apellido2, dni, api_token) VALUES
+INSERT INTO clientes (login, password, nombre, apellido1, apellido2, dni, api_token) VALUES 
 ('TheBarberHub', '1234', 'TheBarberHub', 'Store', 'Main', '12345678A', 'token_barber_001'),
 ('juan123', '1234', 'Juan', 'Pérez', 'García', '12345678B', 'token_juan_001');
 
@@ -63,11 +63,11 @@ INSERT INTO cuentas_bancarias (cliente_id, iban, saldo) VALUES
 (2, 'ES44 8765 4321 4444 4444 4444', 3050.20);
 
 INSERT INTO tarjetas_credito (cuenta_id, numero_tarjeta, fecha_caducidad, cvc, nombre_completo) VALUES
-(1, '4111111111111111', '12/28', '123', 'TheBarberHub Store'),
-(1, '5500000000000004', '07/29', '456', 'TheBarberHub Store'),
-(2, '4000123412341234', '03/27', '321', 'TheBarberHub Store'),
-(2, '5100510051005100', '11/30', '654', 'TheBarberHub Store'),
-(3, '4111222233334444', '09/28', '987', 'Juan Pérez García'),
-(3, '5500999988887777', '01/29', '741', 'Juan Pérez García'),
-(4, '4000999911112222', '05/27', '852', 'Juan Pérez García'),
-(4, '5100123412341234', '10/30', '369', 'Juan Pérez García');
+(1, '4111 1111 1111 1111', '2028-12', '123', 'TheBarberHub Store'),
+(1, '5500 0000 0000 0004', '2029-07', '456', 'TheBarberHub Store'),
+(2, '4000 1234 1234 1234', '2027-03', '321', 'TheBarberHub Store'),
+(2, '5100 5100 5100 5100', '2030-11', '654', 'TheBarberHub Store'),
+(3, '4111 2222 3333 4444', '2028-09', '987', 'Juan Pérez García'),
+(3, '5500 9999 8888 7777', '2029-01', '741', 'Juan Pérez García'),
+(4, '4000 9999 1111 2222', '2027-05', '852', 'Juan Pérez García'),
+(4, '5100 1234 1234 1234', '2030-01', '369', 'Juan Pérez García');
--- sql-init/thebarberhub-init.sql
+++ sql-init/thebarberhub-init.sql
@@ -1,79 +1,159 @@
-DROP DATABASE IF EXISTS db_thebarberhub;
-CREATE DATABASE IF NOT EXISTS db_thebarberhub;
+CREATE DATABASE IF NOT EXISTS thebarberhub;
 
-USE db_thebarberhub;
+USE thebarberhub;
 
 CREATE TABLE usuarios (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(50) NOT NULL UNIQUE,
-contrasenya VARCHAR(100) NOT NULL,
-nombre VARCHAR(25) NOT NULL,
-rol ENUM('Admin', 'Peluqueria', 'Cliente') NOT NULL
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(50) NOT NULL UNIQUE,
+	contrasenya VARCHAR(100) NOT NULL,
+	nombre VARCHAR(25) NOT NULL,
+	rol ENUM('Admin','Peluqueria','Cliente') NOT NULL
 );
 
 CREATE TABLE peluquerias (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-usuario_id BIGINT UNIQUE NOT NULL,
-municipio VARCHAR(15),
-direccion VARCHAR(100) NOT NULL,
-telefono VARCHAR(20) NOT NULL,
-FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	usuario_id BIGINT UNIQUE NOT NULL,
+	municipio VARCHAR(15),
+	direccion VARCHAR(100) NOT NULL,
+	telefono VARCHAR(20) NOT NULL,
+	iban VARCHAR(50) NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE peluquerias_horarios (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	peluqueria_id BIGINT NOT NULL,
+	dia_semana TINYINT NOT NULL,
+	hora_apertura TIME NOT NULL,
+	hora_cierre TIME NOT NULL,
+	FOREIGN KEY (peluqueria_id) REFERENCES peluquerias(id) ON DELETE CASCADE
 );
 
 CREATE TABLE categorias (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR(20) NOT NULL,
-descripcion VARCHAR(100)
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(20) NOT NULL,
+	descripcion VARCHAR(100)
 );
 
 CREATE TABLE productos (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-categoria_id BIGINT NOT NULL,
-peluqueria_id BIGINT NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-precio DECIMAL(10, 2) NOT NULL,
-duracion INT NOT NULL,
-FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE,
-FOREIGN KEY (peluqueria_id) REFERENCES peluquerias(id) ON DELETE CASCADE
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	categoria_id BIGINT NOT NULL,
+	peluqueria_id BIGINT NOT NULL,
+	nombre VARCHAR(20) NOT NULL,
+	precio DECIMAL(10,2) NOT NULL,
+	duracion INT NOT NULL,
+	FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE,
+	FOREIGN KEY (peluqueria_id) REFERENCES peluquerias(id) ON DELETE CASCADE
 );
 
 CREATE TABLE sesion (
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-usuario_id BIGINT NOT NULL,
-token VARCHAR(100) UNIQUE NOT NULL,
-expired_date DATE,
-FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	usuario_id BIGINT NOT NULL,
+	token VARCHAR(100) UNIQUE NOT NULL,
+	expired_date DATE,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE solicitudes (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	usuario_id BIGINT NOT NULL,
+	tipo ENUM('Peluqueria','Producto') NOT NULL,
+	estado ENUM('Pendiente','Aprobada','Rechazada','Confirmada')
+	       DEFAULT 'Pendiente' NOT NULL,
+	fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE solicitudes_peluqueria (
+	id BIGINT PRIMARY KEY,
+	municipio VARCHAR(15),
+	direccion VARCHAR(100) NOT NULL,
+	telefono VARCHAR(20) NOT NULL,
+	FOREIGN KEY (id) REFERENCES solicitudes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE solicitudes_producto (
+	id BIGINT PRIMARY KEY,
+	categoria_id BIGINT NOT NULL,
+	nombre VARCHAR(20) NOT NULL,
+	precio DECIMAL(10,2) NOT NULL,
+	duracion INT NOT NULL,
+	tipo ENUM('Crear','Modificar'),
+	FOREIGN KEY (id) REFERENCES solicitudes(id) ON DELETE CASCADE,
+	FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE reservas (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	cliente_id BIGINT NOT NULL,
+	peluqueria_id BIGINT NOT NULL,
+	dia_semana TINYINT NOT NULL,
+	fecha_reserva DATE NOT NULL,
+	hora_inicio TIME NOT NULL,
+	hora_final TIME,
+	precio_total DECIMAL(10,2) DEFAULT 0.00 NOT NULL,
+	estado ENUM('Pendiente','Completada','Cancelada')
+	       DEFAULT 'Pendiente' NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME,
+	iban VARCHAR(50) NOT NULL,
+	FOREIGN KEY (cliente_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+	FOREIGN KEY (peluqueria_id) REFERENCES peluquerias(id) ON DELETE CASCADE
+);
+
+CREATE TABLE reservas_productos (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	reserva_id BIGINT NOT NULL,
+	producto_id BIGINT NOT NULL,
+	FOREIGN KEY (reserva_id) REFERENCES reservas(id) ON DELETE CASCADE,
+	FOREIGN KEY (producto_id) REFERENCES productos(id)
 );
 
 INSERT INTO usuarios (email, contrasenya, nombre, rol) VALUES
-('admin@thebarberhub.com', '1234', 'Administrador', 'Admin'),
-('peluqueria_valencia@gmail.com', '1234', 'Barbería Valencia', 'Peluqueria'),
-('peluqueria_madrid@gmail.com', '1234', 'Barbería Madrid', 'Peluqueria'),
-('juan.cliente@gmail.com', '1234', 'Juan Pérez', 'Cliente'),
-('maria.cliente@gmail.com', '1234', 'María López', 'Cliente');
+('admin@thebarberhub.com','1234','Administrador','Admin'),
+('peluqueria_valencia@gmail.com','1234','Barbería Valencia','Peluqueria'),
+('peluqueria_madrid@gmail.com','1234','Barbería Madrid','Peluqueria'),
+('juan.cliente@gmail.com','1234','Juan Pérez','Cliente'),
+('maria.cliente@gmail.com','1234','María López','Cliente'),
+('andreualfonso05@gmail.com','1234','Andreu Alfonso','Cliente');
 
-INSERT INTO peluquerias (usuario_id, municipio, direccion, telefono) VALUES
-(2, 'Valencia', 'Calle Colón 25', '600123456'),
-(3, 'Madrid', 'Gran Vía 100', '611987654');
+INSERT INTO peluquerias (usuario_id, municipio, direccion, telefono, iban) VALUES
+(2,'Valencia','Calle Colón 25','600123456','ES5512345678333333333333'),
+(3,'Madrid','Gran Vía 100','611987654','ES4487654321444444444444');
 
 INSERT INTO categorias (nombre, descripcion) VALUES
-('Corte de pelo', 'Servicio de corte y peinado'),
-('Corte de barba', 'Arreglo y perfilado de barba'),
-('Tinte', 'Aplicación de color en el cabello'),
-('Afeitado clásico', 'Afeitado con toalla caliente y navaja');
+('Corte de pelo','Servicio de corte y peinado'),
+('Corte de barba','Arreglo y perfilado de barba'),
+('Tinte','Aplicación de color en el cabello'),
+('Afeitado clásico','Afeitado con toalla caliente y navaja');
 
--- Productos de Barbería Valencia
 INSERT INTO productos (categoria_id, peluqueria_id, nombre, precio, duracion) VALUES
-(1, 1, 'Corte clásico', 12.00, 30),
-(1, 1, 'Corte moderno', 15.00, 40),
-(2, 1, 'Arreglo barba', 10.00, 20),
-(3, 1, 'Tinte completo', 25.00, 60),
-(4, 1, 'Afeitado premium', 18.00, 35);
+(1,1,'Corte clásico',12.00,30),
+(1,1,'Corte moderno',15.00,40),
+(2,1,'Arreglo barba',10.00,20),
+(3,1,'Tinte completo',25.00,60),
+(4,1,'Afeitado premium',18.00,35),
+(1,2,'Corte rápido',10.00,20),
+(1,2,'Corte estilizado',18.00,45),
+(2,2,'Perfilado barba',12.00,25),
+(3,2,'Mechas',30.00,75),
+(4,2,'Afeitado clásico',15.00,30);
 
--- Productos de Barbería Madrid
-INSERT INTO productos (categoria_id, peluqueria_id, nombre, precio, duracion) VALUES
-(1, 2, 'Corte rápido', 10.00, 20),
-(1, 2, 'Corte estilizado', 18.00, 45),
-(2, 2, 'Perfilado barba', 12.00, 25),
-(3, 2, 'Mechas', 30.00, 75),
-(4, 2, 'Afeitado clásico', 15.00, 30);
+INSERT INTO peluquerias_horarios (peluqueria_id, dia_semana, hora_apertura, hora_cierre) VALUES
+(1,1,'10:00:00','14:00:00'),
+(1,1,'16:00:00','20:00:00'),
+(1,2,'10:00:00','14:00:00'),
+(1,2,'16:00:00','20:00:00'),
+(1,3,'10:00:00','14:00:00'),
+(1,3,'16:00:00','20:00:00'),
+(1,4,'10:00:00','20:00:00'),
+(1,5,'10:00:00','20:00:00'),
+(1,6,'10:00:00','14:00:00'),
+(2,1,'10:00:00','14:00:00'),
+(2,1,'16:00:00','20:00:00'),
+(2,2,'10:00:00','14:00:00'),
+(2,2,'16:00:00','20:00:00'),
+(2,3,'10:00:00','20:00:00'),
+(2,4,'10:00:00','20:00:00'),
+(2,5,'10:00:00','20:00:00'),
+(2,6,'10:00:00','14:00:00');
